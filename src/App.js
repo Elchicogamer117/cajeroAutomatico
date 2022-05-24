f1c268ea4599e022fd96f3e0733f04548be70b70
Cristian <cris18002011@hotmail.com>
2022-05-24 14:24:05 -0500

    Solucion 2 - Arregando funcionamiento de estados

--- src/App.js
+++ src/App.js
@@ -34,18 +34,24 @@ function App() {
 //logica del cajero
   const outMoney = (outCash) => { 
     const availableCash = cash - outCash;
-
     setCash(availableCash);
+    //*? Iterando manualmente dado que el useState guarda el estados despues de correr todo el evento y se obtenia un click de retraso en el cambio
+    const select500 = parseInt(outCash / 500);
+    setOB500(select500);
 
-    setOB500(parseInt((outCash / 500)));
-    setOB200(parseInt(((outCash % 500) / 200)))
-    setOB100(parseInt((outCash % 200 / 100)));
-    setOB50(parseInt((outCash % 100 / 50)));
-    
-    setAB500(parseInt(aB500 - parseInt((outCash / 500))));
-    setAB200(parseInt(aB200 - parseInt((outCash % 500 / 200))));
-    setAB100(parseInt(aB100 - parseInt((outCash % 200 / 100))));
-    setAB50(parseInt(aB50 - parseInt((outCash % 100 / 50))));
+    const select200 = parseInt((outCash - 500*select500) / 200);
+    setOB200(select200)
+
+    const select100 = parseInt((outCash - 200*select200 - 500*select500)/ 100);
+    setOB100(select100)
+
+    const select50 = parseInt((outCash - 100*select100 - 200*select200 - 500*select500)/50);
+    setOB50(select50)
+
+    setAB500(parseInt(aB500 - select500));
+    setAB200(parseInt(aB200 - select200));
+    setAB100(parseInt(aB100 - select100));
+    setAB50(parseInt(aB50 - select50));
 
     console.log(2000%500)
 };
