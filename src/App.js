96cede500aa19c44cb28dec0f29f7a155c9b3541
Cristian <cris18002011@hotmail.com>
2022-05-24 11:25:21 -0500

    Generando funcionamiento de la aplicación

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const[cash,setCash] = React.useState(12500)
+  const[newCash,setNewCash] = React.useState(0)
+
+  //obtener los valores de entrada
+  const onChange = (event) => {
+    setNewCash(event.target.value)
+    console.log(setCash)
+  }
+  //guardar el nombre en una variable
+  const onSubmit = (event) => {
+    event.preventDefault();
+    let outCash = newCash;
+    console.log(newCash);
+    availableMoney(outCash);
+};
+
+const availableMoney = (outCash) => {
+  const availableCash = cash - outCash;
+  setCash(availableCash);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={onSubmit}>
+      <p>{"Dinero disponible: " + cash }</p>
+      <label> Efectivo a retirar </label>
+      <input 
+      // value={cash}
+      onChange = {onChange}
+      placeholder='Solo enteros'
+      type="submmit" >
+      </input>
+      <button >
+        Retirar
+      </button>
+      <p> </p>
+    </form>
+      
   );
 }
 
